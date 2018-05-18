a4428f7f8c14e4073ccb3cae006666f2290c6163
Tammy Tian <tiantammy@outlook.com>
2018-05-18 23:18:30 +0200

    importing new prop for modularization

--- src/components/guess-count.js
+++ src/components/guess-count.js
@@ -3,6 +3,7 @@ import React from 'react';
 import './guess-count.css';
 
 export default function GuessCount(props) {
+    
     return (
         <p>
             Guess #<span id="count">{props.count}</span>!
